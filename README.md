2ce59ffeb9c5b9c3bc578e9410707c2159702507
LW83 <ljbwilkinson@gmail.com>
2022-07-06 10:42:23 +0000

    Add validation for name and age inputs.

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
-https://ascii-generator.site/
+https://ascii-generator.site/ - to create ASCII version of images
+https://stackoverflow.com/questions/23623288/print-full-ascii-art - how to insert ASCII images into Python
+https://www.codespeedy.com/check-if-user-input-is-a-string-or-number-in-python/ - to check name input for digits instead of letters
 
 
 
--- run.py
+++ run.py
@@ -1,3 +1,4 @@
+from datetime import datetime 
 import gspread
 from google.oauth2.service_account import Credentials
 
@@ -12,84 +13,119 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('hogwarts_houses')
 
+year = datetime.now().year
 houses = SHEET.worksheet('Houses')
 data = houses.get_all_values()
 
-print(r"""
-=                             
-=:                            
-:+                            
-.#:                           
- *+                           
- =#-                          
- -##.                         
- :##=                         
-  ###.                        
-  *##=                :.      
-  =###.            .=*#+      
-  -###+          :+#####.     
-  .####-      :=*#######-     
-   ####*   .=*##########+     
-   +####--*##############.    
-   -##############**#####-    
-   .###########*=. .#####+    
-    *#######+-.     .#####.   
-    +####+:          -####=   
-    -*=:              +####.  
-                      .*###:  
-                       .*##+  
-                        :###  
-                         =##: 
-                          +#= 
-                           ** 
-                           :#:
-                            ==
-                             +
-                             """)
+def intro_logo_1():
+    print(r"""
+    =                             
+    =:                            
+    :+                            
+    .#:                           
+    *+                           
+    =#-                          
+    -##.                         
+    :##=                         
+      ###.                        
+      *##=                :.      
+      =###.            .=*#+      
+      -###+          :+#####.     
+      .####-      :=*#######-     
+      ####*   .=*##########+     
+      +####--*##############.    
+      -##############**#####-    
+      .###########*=. .#####+    
+        *#######+-.     .#####.   
+        +####+:          -####=   
+        -*=:              +####.  
+                          .*###:  
+                          .*##+  
+                            :###  
+                            =##: 
+                              +#= 
+                              ** 
+                              :#:
+                                ==
+                                +
+                                """)
 
-print(r"""
-                                    =++++=====.             
-                                    +@@@@@@@@@: .-+*:       
-                      :---------=   :@@@@@@@@@%@@%%@@#.     
-       .........::     =@@@@@@@@-    @@@@@@@@@=   .@@@@+    
-       -%@@@@@@@@=     .@@@@@@@*     #@@@@@@@@:    %@@@@%:  
-        :@@@@@@@@       @@@@@@@+     +@@@@@@@@:    *@@@@@@* 
-        :@@@@@@@@       %@@@@@@=     :@@@@@@@@:    =@@@@@@@:
-        .@@@@@@@@       #@@@@@@=      @@@@@@@@:    -@@@@@@@.
-        .@@@@@@@@       #@@@@@@=      #@@@@@@@:    :@@@@@@@ 
-         @@@@@@@@       *@@@@@@=      =@@@@@@@-    .@@@@@@# 
-         @@@@@@@@       *@@@@@@+      .@@@@@@@-    .@@@@@@+ 
-         %@@@@@@@       *@@@@@@+:--=-. @@@@@@@-    .@@@@@@- 
-   .:-   %@@@@@@@       *@@@@@@@%*+=%@%@@@@@@@=*-  .@@@@@@- 
- #@+:    #@@@@@@@.    :*@@@@@@@*    =@@@@@@@@@=.%#=@@@@@%=  
- .##=.   #@@@@@@@:.-*@@@@@@@@@@*    .@@@@@@@@@+  *@%#+-.    
-   =%@%+-#@@@@@@@@@@@@%+#@@@@@@*     %@@@@@@@@*             
-     .-+%@@@@@@@@#+=:   =@@@@@@*     +@@@@@@@@%             
-         +@@@@@@@-      =@@@@@@*     :@@@@@@%@@             
-         +@@@@@@@-      -@@@@@@*      @@@@@@==@.            
-         +@@@@@@@-      -@@@@@@*    +-#@@@@@= =.            
-         +@@@@@@@-      -@@@@@@*    -@@@@@@@=               
-         *@@@@@@@-      -@@@@@@*     @@@@@@@=               
-         #@@@@@@@=      =@@@@@@*     #@@@@@@=               
-        -@@@@@@@@=      *@@@@@@*     -@@@@@@=               
-      =#%@@@@@@@@%:     %@@@@@@#      @@@%+@=               
-                  ..  .#@@@@@@@@:     #@@# ==               
-                     -*******##%%+.   -@@*                  
-                                       @@+                  
-                                       #@-                  
-                                       -@:                  
-                                        @                   
-                                        *     """)
+def intro_logo_2():
+    print(r"""
+                                        =++++=====.             
+                                        +@@@@@@@@@: .-+*:       
+                          :---------=   :@@@@@@@@@%@@%%@@#.     
+          .........::     =@@@@@@@@-    @@@@@@@@@=   .@@@@+    
+          -%@@@@@@@@=     .@@@@@@@*     #@@@@@@@@:    %@@@@%:  
+            :@@@@@@@@       @@@@@@@+     +@@@@@@@@:    *@@@@@@* 
+            :@@@@@@@@       %@@@@@@=     :@@@@@@@@:    =@@@@@@@:
+            .@@@@@@@@       #@@@@@@=      @@@@@@@@:    -@@@@@@@.
+            .@@@@@@@@       #@@@@@@=      #@@@@@@@:    :@@@@@@@ 
+            @@@@@@@@       *@@@@@@=      =@@@@@@@-    .@@@@@@# 
+            @@@@@@@@       *@@@@@@+      .@@@@@@@-    .@@@@@@+ 
+            %@@@@@@@       *@@@@@@+:--=-. @@@@@@@-    .@@@@@@- 
+      .:-   %@@@@@@@       *@@@@@@@%*+=%@%@@@@@@@=*-  .@@@@@@- 
+    #@+:    #@@@@@@@.    :*@@@@@@@*    =@@@@@@@@@=.%#=@@@@@%=  
+    .##=.   #@@@@@@@:.-*@@@@@@@@@@*    .@@@@@@@@@+  *@%#+-.    
+      =%@%+-#@@@@@@@@@@@@%+#@@@@@@*     %@@@@@@@@*             
+        .-+%@@@@@@@@#+=:   =@@@@@@*     +@@@@@@@@%             
+            +@@@@@@@-      =@@@@@@*     :@@@@@@%@@             
+            +@@@@@@@-      -@@@@@@*      @@@@@@==@.            
+            +@@@@@@@-      -@@@@@@*    +-#@@@@@= =.            
+            +@@@@@@@-      -@@@@@@*    -@@@@@@@=               
+            *@@@@@@@-      -@@@@@@*     @@@@@@@=               
+            #@@@@@@@=      =@@@@@@*     #@@@@@@=               
+            -@@@@@@@@=      *@@@@@@*     -@@@@@@=               
+          =#%@@@@@@@@%:     %@@@@@@#      @@@%+@=               
+                      ..  .#@@@@@@@@:     #@@# ==               
+                        -*******##%%+.   -@@*                  
+                                          @@+                  
+                                          #@-                  
+                                          -@:                  
+                                            @                   
+                                            *     """)
 
-name = input("Please enter your Name:\n")
-age = input(f"Hi {name}, please tell us your age:\n")
-country = input("Thank you, finally please tell us what Country you are from:\n")
+def welcome():
+    """
+    """
+    name = input("Please enter your Name:\n")
+    validate_name(name)
+    age = input(f"Hi {name}, please tell us your age:\n")
+    validate_age(int(age))
+    country = input("Thank you, finally, please tell us what Country you are from:\n")
 
-print("Confirming non-Muggle status........\n")
-print("Non-muggle status validated\n")
+    print("Confirming non-Muggle status........\n")
+    print("Non-muggle status validated\n")
+    print(f"Welcome to Howgwarts School of Witchcraft and Wizardry {name}. \nWe are delighted to have you join us for the {year} school term.\n")
+    print("In order to place you in the correct house for your time with us, the Sorting Hat needs to know a little more about you...")
+    print(f"So, {name} are you ready to get started?")
 
-print(f"Welcome to Howgwarts School of Witchcraft and Wizardry {name}. \nWe are delighted to have you join us for the XXXX school term.\n")
+def validate_name(name):
+    """
+    Raises error if name entered has numerical entries.
+    """
+    try: 
+      if name.isnumeric():
+        raise ValueError(
+          f"Please enter your name as text, you entered {name}"
+        )
+    except ValueError as e:
+      print(f"{e}, please try again.\n")
 
-print("In order to place you in the correct house for your time with us, the Sorting Hat needs to know a little more about you...")
+def validate_age(number):
+    """
+    Raises error if age entered is over 18.
+    """
+    try: 
+      if number > 18:
+        raise ValueError(
+          f"{number}! Those over 18 are above the age of acceptance for Hogwarts"
+        )
+    except ValueError as e:
+      print(f"{e}, please re-count your years and try again.\n")
 
-print(f"So, {name} are you ready to get started?")
+def main():
+    intro_logo_2()
+    welcome()
+
+main()
