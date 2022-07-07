62a200d20dba972ff5ab07a9f3e0a2349daafba6
LW83 <ljbwilkinson@gmail.com>
2022-07-07 11:24:53 +0000

    Wire up question loop and answer validation function

--- questions.py
+++ questions.py
@@ -4,78 +4,78 @@ Sorting hat questions
 
 questions = [
     
-    "You are given a team project to complete\n"
-    "in school. What role do you play in the team?\n"
-    "(a) The organiser, assigning roles and tasks"
-    "to others in the team"
-    "(b) Team projects are great, I can let everyone"
-    "else do all the work!"
-    "(c) I work hard to make sure I am a good member"
-    "of the team."
-    "(d) I work away on my own, researching the best"
-    "options.",
+    {'question': "You are given a team project to complete\n"
+    "in school. What role do you play in the team?\n",
+    'options': ["The organiser, assigning roles and tasks"
+    "to others in the team\n",
+    "Team projects are great, I can let everyone "
+    "else do all the work!\n",
+    "I work hard to make sure I am a good member "
+    "of the team.\n",
+    "I work away on my own, researching the best"
+    "options.\n"]},
 
-    "Its sports day and you are competing in a number\n"
-    "of events. Do you.."
-    "(a) Put in lots of practice in the weeks before"
-    "You want to win!"
-    "(b) Do whatever it takes to win..if that means"
-    "tripping someone esle up so be it!"
-    "(c) Give it my best effort but at the end of the"
-    "day the best boy or girl wins!"
-    "(d) Sports day? Uggh can we not do soemthing more"
-    "interesting?!.",
+    {'question': "Its sports day and you are competing in a number\n"
+    "of events. Do you..\n",
+    'options' : ["Put in lots of practice in the weeks before"
+    "You want to win!\n",
+    "Do whatever it takes to win..if that means"
+    "tripping someone esle up so be it!\n",
+    "Give it my best effort but at the end of the"
+    "day the best boy or girl wins!\n",
+    "Sports day? Uggh can we not do soemthing more"
+    "interesting?!.\n"]},
 
-    "You see another pupil being teased about their clothes."
-    "Do you.."
-    "(a) Threaten the bullies and tell them to back off."
-    "(b) Laugh and maybe join in."
-    "(c) Find the pupil afterwards and talk to them."
-    "(d) Feel sorry for them but mind your own business.",
+    {'question': "You see another pupil being teased about their clothes."
+    "Do you..\n",
+    'options': ["Threaten the bullies and tell them to back off.\n",
+    "Laugh and maybe join in.\n",
+    "Find the pupil afterwards and talk to them.\n",
+    "Feel sorry for them but mind your own business.\n"]},
 
-    "Which profession appeals to you the most.."
-    "(a) "
-    "(b) "
-    "(c) "
-    "(d) " , 
+    {'question': "Which profession appeals to you the most..\n",
+    'options': ["(a) \n",
+    "(b) \n",
+    "(c) \n",
+    "(d) \n"]},
 
-    "You went to HoneyDukes to buy some sweets but"
-    "realised later you were undercharged. Do you.."
-    "(a) Go straight back to tell them and pay the "
-    "correct amount"
-    "(b) Think brilliant! Their mistake is my win!"
-    "(c) Remember to go back next time you are in"
-    "Diagon Alley and pay the difference"
-    "(d) Think oh that's lucky!",   
+    {'question': "You went to HoneyDukes to buy some sweets but "
+    "realised later you were undercharged. Do you..\n",
+    'options': ["Go straight back to tell them and pay the "
+    "correct amount.\n",
+    "Think brilliant! Their mistake is my win!\n",
+    "Remember to go back next time you are in\n"
+    "Diagon Alley and pay the difference\n",
+    "Think oh that's lucky!\n"]},   
 
-    "What is your ideal position in a game of Quidditch?"
-    "(a) Seeker"
-    "(b) Beater"
-    "(c) Chaser"
-    "(d) Team supporter - I am happy in the stands thanks",  
+    {'question': "What is your ideal position in a game of Quidditch?\n",
+    'options': ["Seeker\n",
+    "Beater\n",
+    "Chaser\n",
+    "Team supporter - I am happy in the stands thanks\n"]},  
 
-    "Someone is calling you names. Which do you find most"
-    "upsetting?"
-    "(a) Weak"
-    "(b) Boring"
-    "(c) Cheater"
-    "(d) Stupid", 
+    {'question': "Someone is calling you names. Which do you find most "
+    "upsetting?\n",
+    'options': ["Weak\n",
+    "Boring\n",
+    "Cheater\n",
+    "Stupid\n"]}, 
 
-    "You value friends that are..."
-    "(a) Brave and adventurous"
-    "(b) Popular and admired"
-    "(c) Kind and loyal"
-    "(d) Intelligent", 
+    {'question': "You value friends that are...\n",
+    'options': ["Brave and adventurous\n",
+    "Popular and admired\n",
+    "Kind and loyal\n",
+    "Intelligent\n"]}, 
 
-    "You are most likely to get in trouble for..."
-    "(a) Punching a bully"
-    "(b) Cheating on an exam"
-    "(c) I don't get in trouble!"
-    "(d) Staying up late reading", 
+    {'question': "You are most likely to get in trouble for...\n",
+    'options': ["Punching a bully\n",
+    "Cheating on an exam\n",
+    "I don't get in trouble!\n",
+    "Staying up late reading\n"]},
 
-    "Placeholder."
-    "(a) "
-    "(b) "
-    "(c) "
-    "(d) ", 
+    {'question': "Placeholder.\n",
+    'options': ["(a) \n",
+    "(b) \n",
+    "(c) \n",
+    "(d) \n"]} 
 ]
--- run.py
+++ run.py
@@ -23,36 +23,36 @@ data = houses.get_all_values()
 
 def intro_logo_1():
     print(Fore.YELLOW + r"""
-   =                             
-   =:                            
-   :+                            
-   .#:                           
-    *+                           
-    =#-                          
-    -##.                         
-    :##=                         
-     ###.                        
-     *##=                :.      
-     =###.            .=*#+      
-     -###+          :+#####.     
-     .####-      :=*#######-     
-      ####*   .=*##########+     
-      +####--*##############.    
-      -##############**#####-    
-      .###########*=. .#####+    
-       *#######+-.     .#####.   
-       +####+:          -####=   
-       -*=:              +####.  
-                         .*###:  
-                          .*##+  
-                           :###  
-                            =##: 
-                             +#= 
-                              ** 
-                              :#:
-                               ==
-                                +
-                                """)
+       =                             
+       =:                            
+       :+                            
+       .#:                           
+        *+                           
+        =#-                          
+        -##.                         
+        :##=                         
+         ###.                        
+         *##=                :.      
+         =###.            .=*#+      
+         -###+          :+#####.     
+         .####-      :=*#######-     
+          ####*   .=*##########+     
+          +####--*##############.    
+          -##############**#####-    
+          .###########*=. .#####+    
+           *#######+-.     .#####.   
+           +####+:          -####=   
+           -*=:              +####.  
+                             .*###:  
+                              .*##+  
+                               :###  
+                                =##: 
+                                 +#= 
+                                  ** 
+                                  :#:
+                                   ==
+                                    +
+                                    """.center(70))
 
 def intro_logo_2():
     print(Fore.BLUE +r"""
@@ -105,7 +105,6 @@ def welcome():
             break
     
     sleep(1)
-    clear_display()
 
     while True:
         age = input(f"\nPlease tell me your age:".center(70))
@@ -115,37 +114,61 @@ def welcome():
             break
     
     sleep(1)
-    clear_display()
-
-    country = input("\nFinally, please tell us what Country you are from:\n".center(70))
+    country = input("\nFinally, please tell us what Country you are from:".center(70))
   
     sleep(2)
     clear_display()
 
-    print("\nConfirming non-Muggle status........\n".center(70))
+    print("Confirming non-Muggle status........\n".center(70))
     sleep(3)
     print("Non-Muggle status VALIDATED\n".center(70))
     sleep(2)
+    clear_display()
+    intro_logo_1()
+    print(Style.RESET_ALL)
     print(f"Welcome to Hogwarts School of Witchcraft and Wizardry {name}.\n".center(70))
     sleep(2)
     print(f"We are delighted to have you join us for the {year} school term.\n".center(70))
     sleep(3)
     print("In order to place you in the correct house for your time with us, ".center(70))
     print("the Sorting Hat needs to know a little more about you...\n".center(70))
-    sleep(2)
+    sleep(3)
     print(f"So, {name}, are you ready to get started?\n".center(70))
 
 def start_sorting():
     sort = input("Please enter y for 'Let's get sorted!' or n if you wish to remove the sorting hat.".center(70))
     if sort == 'n':
-        print(f"You have removed the sorting hat. Goodbye {name} maybe we will see you next term instead.".center(70))
+        print(f"\n\nYou have removed the sorting hat. Goodbye NAME maybe we will see you next term instead.".center(70))
+        sleep (3)
+        clear_display()
+        intro_logo_2()
+        print(Style.RESET_ALL)
         welcome()
     elif sort == 'y':
         generate_questions()
     else:
-        print("\nPlease enter either 'y' or 'n' in order to proceed.".center(70))
+        print("\nPlease enter either 'y' or 'n' in order to proceed.\n".center(70))
 
-#def generate_questions():#
+def generate_questions():
+    """
+    Runs through sorting hat questions
+    """
+    clear_display()
+    for q in questions:  #https://stackoverflow.com/questions/33069253/looping-through-multiple-choice-questions
+        print(q['question'])
+        sleep(1)
+        for i, c in enumerate(q['options']):
+            print(chr(97 + i)+ ':', c)
+            
+        while True: 
+            answer = input("Enter your answer:\n>>> ".center(70))
+    
+            if validate_answer(answer):
+                print('\nThank you'.center(70))
+                break
+
+        sleep(1)
+        clear_display()
 
 def clear_display(): #dnlBowers
     """"
@@ -179,11 +202,15 @@ def validate_age(number):
     try: 
       if number > 18:
         raise ValueError(
-          f"{number}! Those over 18 are above the age of acceptance for Hogwarts"
+          f"\n{number}! Hogwarts cannot accept students over the age of 18"
         )
       elif number < 4:
         raise ValueError(
-          f"Unfortunately Hogwarts can only accept students over the age of 4. As you are only {number} you will need to wait a few years and come back."
+          f"\nHogwarts can only accept students over the age of 4. As you are only {number} you will need to wait a few years and come back."
+        )
+      elif number == '':
+        raise ValueError(
+          "Please tell us your age so we are sure you can be accepted to Hogwarts."
         )
       else:
         return True
@@ -191,11 +218,32 @@ def validate_age(number):
     except ValueError as e:
       print(f"{e} Please re-count your years and try again.\n")
       return False
+
+def validate_answer(answer):
+    """
+    Raises error if answer entered is not a, b, c, or d.
+    """
+    try: 
+      if answer not in {'a','b','c','d'}:
+        raise ValueError(
+          f"You answered {answer},"
+        )
+      elif answer == '':
+        raise ValueError(
+          "You did not provide an answer,"
+        )
+      else:
+        return True
+
+    except ValueError as e:
+      print(f"{e} please select a, b, c or d as your answer to proceed.\n")
+      return False
     
 def main():
     intro_logo_2()
     print(Style.RESET_ALL)
     welcome()
     start_sorting()
+    generate_questions()
 
 main()
